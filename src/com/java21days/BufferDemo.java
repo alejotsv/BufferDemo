9986243cd676db4829b1a6f1169b6127b753b719
Alejandro Salas <asalas.wd@outlook.com>
2022-11-30 19:50:39 -0500

    Initial commit

---
+++ src/com/java21days/BufferDemo.java
@@ -0,0 +1,14 @@
+package com.java21days;
+
+public class BufferDemo {
+    public static void main(String[] args) {
+        int start = 0;
+        int finish = 255;
+        if(args.length > 1){
+            start = Integer.parseInt(args[0]);
+            finish = Integer.parseInt(args[1]);
+        } else if(args.length > 0){
+            start = Integer.parseInt(args[0]);
+        }
+    }
+}
